b53155ef5fbf31d8719563e871e8970c3625e911
yjleong <62537656+yjleong@users.noreply.github.com>
2020-12-06 20:39:11 -0700

    Added exceptions for not getting transactions from etherscan properly

--- DEXTaxTool_ConsoleApp/ClassLibrary2/EtherScanTxnMapper.cs
+++ DEXTaxTool_ConsoleApp/ClassLibrary2/EtherScanTxnMapper.cs
@@ -24,16 +24,33 @@ namespace Parser
             {
                 case TxnTypeEnum.Normal:
                     NormalTxnResponse normalTxnResponse = JsonConvert.DeserializeObject<NormalTxnResponse>(JsonStr);
-                    //if (normalTxnResponse.status == "1")
+                    if (normalTxnResponse.status == "0")
+                    {
+                        throw getErrorMessage(JsonStr);
+                    }
                     return normalTxnResponse.result;
                 case TxnTypeEnum.Erc20:
                     Erc20TxnResponse erc20TxnResponse = JsonConvert.DeserializeObject<Erc20TxnResponse>(JsonStr);
+                    if (erc20TxnResponse.status == "0")
+                    {
+                        throw getErrorMessage(JsonStr);
+                    }
                     return erc20TxnResponse.result;
                 case TxnTypeEnum.Internal:
                     InternalTxnResponse internalTxnResponse = JsonConvert.DeserializeObject<InternalTxnResponse>(JsonStr);
+                    if (internalTxnResponse.status == "0")
+                    {
+                        throw getErrorMessage(JsonStr);
+                    }
                     return internalTxnResponse.result;
             }
             throw new Exception("MapToTxn(): unrecognizable TxnTypeEnum. Can't deserialize");
+        }
+        private Exception getErrorMessage(string JsonStr)
+        {
+            var definition = new { status = "", message = "", result = "" };
+            var errorJson = JsonConvert.DeserializeAnonymousType(JsonStr, definition);
+            return new Exception($"Problem with getting JSON from EtherScan \n Message : {errorJson.message} Result: {errorJson.result}");
         }
 
     }
